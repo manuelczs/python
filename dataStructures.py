ea277e6473c59ef6382e0e6a70a21146b81640ce
manuelczs <manuelczs@outlook.com>
2018-09-05 02:18:25 -0300

    more functions added

--- dataStructures.py
+++ dataStructures.py
@@ -6,3 +6,12 @@ class Stack(object):
 
 	def size(self):
 		return len(self.items)
+
+	def pop(self):
+		return self.items.pop()
+
+	def peak(self):
+		return self.items[len(self.items) - 1]
+
+	def isEmpty(self):
+		return self.items == []
