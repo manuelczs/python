d4c317599581623ffb180390d8d619812c65e7d3
manuelczs <manuelczs@outlook.com>
2018-10-21 17:51:52 -0300

    webscrap

--- webscrap.py
+++ webscrap.py
@@ -1,6 +1,25 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-bsObj = BeautifulSoup(html.read(), 'html5lib')
-print(bsObj.h1)
+#html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+
+def getTitle(url):
+  # first try get url
+  try:
+    html = urlopen(url)
+  except HTTPError as e:
+    return None
+  try:
+    bsObj = BeautifulSoup(html.read())
+    title = bsObj.body.h1
+  except AtributeError as e:
+    return None
+  return title
+
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+
+if title == None:
+  print('Title not found!')
+else:
+  print(title)
